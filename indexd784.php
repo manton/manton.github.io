c54f625889a13f0be9d2c5782e8a6b3da6dea39c
Manton Reece <manton@me.com>
2016-12-13 16:34:44 -0600

    Syncing up the latest web version. December 2016.

---
+++ indexd784.php
@@ -0,0 +1,184 @@
+<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"
+	xmlns:content="http://purl.org/rss/1.0/modules/content/"
+	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+	xmlns:dc="http://purl.org/dc/elements/1.1/"
+	xmlns:atom="http://www.w3.org/2005/Atom"
+	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	>
+
+<channel>
+	<title>Manton Reece</title>
+	<atom:link href="http://www.manton.org/index.php?feed=rss2" rel="self" type="application/rss+xml" />
+	<link>http://www.manton.org</link>
+	<description></description>
+	<lastBuildDate>Tue, 13 Dec 2016 15:09:38 +0000</lastBuildDate>
+	<language>en-US</language>
+	<sy:updatePeriod>hourly</sy:updatePeriod>
+	<sy:updateFrequency>1</sy:updateFrequency>
+	<generator>https://wordpress.org/?v=4.6.1</generator>
+	<item>
+		<title>Where I post everything</title>
+		<link>http://www.manton.org/2016/12/where-i-post-everything.html</link>
+		<pubDate>Tue, 13 Dec 2016 15:09:38 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Weblogs]]></category>
+		<category><![CDATA[blogging]]></category>
+		<category><![CDATA[feed]]></category>
+		<category><![CDATA[microblog]]></category>
+		<category><![CDATA[photos]]></category>
+		<category><![CDATA[rss]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4555</guid>
+		<description><![CDATA[My content is all on this blog or linked from it, but if you&#8217;re following RSS feeds or Twitter it&#8217;s not as obvious where everything is posted. Here&#8217;s a summary to clear things up. Microblog posts: Posted here and in a special RSS feed. Also automatically cross-posted to Twitter and App.net, with some occasional truncation. [&#8230;]]]></description>
+				<content:encoded><![CDATA[<p>My content is all on this blog or linked from it, but if you&#8217;re following RSS feeds or Twitter it&#8217;s not as obvious where everything is posted. Here&#8217;s a summary to clear things up.</p>
+<p><strong>Microblog posts:</strong> Posted here and in <a href="http://www.manton.org/snippets.xml">a special RSS feed</a>. Also automatically cross-posted to <a href="https://twitter.com/manton2">Twitter</a> and App.net, with some occasional truncation.</p>
+<p><strong>Longer posts:</strong> Posted here and in <a href="http://www.manton.org/rss.xml">the default RSS feed</a>. Also automatically cross-posted to Twitter and App.net with the title and link. Twitter cross-posting is handled by my upcoming <a href="http://micro.blog/">Micro.blog</a> platform.</p>
+<p><strong>Photos:</strong> Posted <a href="https://www.instagram.com/manton/">to Instagram</a> and then copied here <a href="http://www.manton.org/2016/03/blogging-your-photos.html">using this workflow</a>. They don&#8217;t show up in either of the RSS feeds above. They&#8217;re not cross-posted to Twitter.</p>
+<p><strong>Timetable:</strong> Posted to <a href="http://timetable.fm/">timetable.fm</a> which has its own feed. Discoverable in your favorite podcast client.</p>
+<p><strong>Core Intuition:</strong> Posted to <a href="http://www.coreint.org/">coreint.org</a>. I&#8217;ll usually post a link here on the blog for each new episode.</p>
+<p><strong>All posts:</strong> Switching to WordPress brought a new global RSS feed, but I redirect it to the longer posts for now. There&#8217;s a new <a href="http://www.manton.org/everything.xml">everything RSS feed</a> which contains all posts: microblog, full posts, and photos. Enjoy!</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title></title>
+		<link>http://www.manton.org/2016/12/4553.html</link>
+		<pubDate>Tue, 13 Dec 2016 14:47:57 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Snippets]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4553</guid>
+		<description><![CDATA[AirPods ready to order. I&#8217;ll pass for now. Bluetooth still has downsides to me, and my iPhone SE has a perfectly reliable headphone jack.]]></description>
+				<content:encoded><![CDATA[<p>AirPods ready to order. I&#8217;ll pass for now. Bluetooth still has downsides to me, and my iPhone SE has a perfectly reliable headphone jack.</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title></title>
+		<link>http://www.manton.org/2016/12/4551.html</link>
+		<pubDate>Tue, 13 Dec 2016 13:42:44 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Snippets]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4551</guid>
+		<description><![CDATA[Starting the day off with my pre-holiday morning work routine: iced coffee and &#8220;My Shot&#8221; from the Hamilton mixtape.]]></description>
+				<content:encoded><![CDATA[<p>Starting the day off with my pre-holiday morning work routine: iced coffee and &#8220;My Shot&#8221; from the Hamilton mixtape.</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title></title>
+		<link>http://www.manton.org/2016/12/4549.html</link>
+		<pubDate>Mon, 12 Dec 2016 20:40:56 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Snippets]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4549</guid>
+		<description><![CDATA[Worked at the new WeWork location at the Domain this morning. Nice place. Back home and just published an episode of my Timetable podcast.]]></description>
+				<content:encoded><![CDATA[<p>Worked at the new WeWork location at the Domain this morning. Nice place. Back home and just published an episode of my Timetable podcast.</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title></title>
+		<link>http://www.manton.org/2016/12/4546.html</link>
+		<pubDate>Sun, 11 Dec 2016 16:04:48 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Snippets]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4546</guid>
+		<description><![CDATA[Now that I&#8217;ve been cross-posting Instagram photos to my blog for a while, finally fixed the CSS on small screens.]]></description>
+				<content:encoded><![CDATA[<p>Now that I&#8217;ve been cross-posting <a href="http://www.manton.org/category/photos">Instagram photos to my blog</a> for a while, finally fixed the CSS on small screens.</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title>Next year&#8217;s Christmas trees</title>
+		<link>http://www.manton.org/2016/12/next-years-christmas-trees.html</link>
+		<pubDate>Sun, 11 Dec 2016 01:20:09 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Photos]]></category>
+		<category><![CDATA[instagram]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4542</guid>
+		<description><![CDATA[At the Elgin Christmas tree farm.]]></description>
+				<content:encoded><![CDATA[<p><img src="http://www.manton.org/images/2016/next_years_christmas_trees.jpg" width="500" height="500" alt="Next year's Christmas trees"></p>
+<p>At the Elgin Christmas tree farm.</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title>Apple Maps adds ChargePoint</title>
+		<link>http://www.manton.org/2016/12/apple-maps-adds-chargepoint.html</link>
+		<pubDate>Fri, 09 Dec 2016 20:16:52 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Technology]]></category>
+		<category><![CDATA[cars]]></category>
+		<category><![CDATA[electric]]></category>
+		<category><![CDATA[maps]]></category>
+		<category><![CDATA[travel]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4540</guid>
+		<description><![CDATA[A few of years ago we took a vacation to New York City and Montreal. We were taking the subway so often, I switched to Google Maps for its transit directions. I&#8217;ve been using Google Maps exclusively ever since. Until now, there were very few reasons to go back to Apple Maps. Apple has been [&#8230;]]]></description>
+				<content:encoded><![CDATA[<p>A few of years ago we took a vacation to New York City and Montreal. We were taking the subway so often, I switched to Google Maps for its transit directions. I&#8217;ve been using Google Maps exclusively ever since.</p>
+<p>Until now, there were very few reasons to go back to Apple Maps. Apple has been playing catch-up. <a href="https://blog.asmartbear.com/complex-decisions.html">Why use a product</a> that is only adding features from a competitor, but not anything new?</p>
+<p>This week Apple rolled out a unique feature that&#8217;s interesting to me: ChargePoint integration to find charging locations for electric cars. Ryan Christoffel covers it <a href="https://www.macstories.net/linked/apple-maps-adds-chargepoint-integration/">at MacStories</a>:</p>
+<blockquote><p>
+  Having spent several years building partnerships to ensure its data won&#8217;t lead any drivers astray, Apple has more recently been able to focus on integrating data that&#8217;s less important, but still quite useful. A few months ago we saw the company <a href="https://www.macstories.net/linked/parkopedia-and-apple-strike-maps-deal/">team up with Parkopedia</a> to improve parking data, and now charging stations are a natural next step.
+</p></blockquote>
+<p>I rarely need this — and the ChargePoint app itself has more detail, such as how many spots are actually available — but I&#8217;m excited about it as a new feature. I hope that it represents a fundamental improvement across the maps platform. I&#8217;m putting Apple Maps back on my home screen for a while.</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title></title>
+		<link>http://www.manton.org/2016/12/4538.html</link>
+		<pubDate>Fri, 09 Dec 2016 18:19:24 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Snippets]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4538</guid>
+		<description><![CDATA[Posted episode 30 of my short podcast, Timetable. Still don&#8217;t have a consistent schedule for this. Maybe Monday and Friday would be good.]]></description>
+				<content:encoded><![CDATA[<p>Posted episode 30 of my short podcast, Timetable. Still don&#8217;t have a consistent schedule for this. Maybe Monday and Friday would be good.</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title>Fake news as propaganda</title>
+		<link>http://www.manton.org/2016/12/fake-news-as-propaganda.html</link>
+		<pubDate>Fri, 09 Dec 2016 16:20:19 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[User Experience]]></category>
+		<category><![CDATA[fakenews]]></category>
+		<category><![CDATA[hillary]]></category>
+		<category><![CDATA[microblogging]]></category>
+		<category><![CDATA[twitter]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4536</guid>
+		<description><![CDATA[In yesterday&#8217;s essay about Twitter, I also linked to my post on Instagram&#8217;s lack of native reposts. Jason Brennan has written a follow-up about fake news and propaganda, exploring what we can learn and apply to microblogging: Aside from the normal reasons propaganda exists, it exists on social networks like Facebook and Twitter because it [&#8230;]]]></description>
+				<content:encoded><![CDATA[<p><a href="http://www.manton.org/2016/12/twitter-at-10-years.html">In yesterday&#8217;s essay about Twitter</a>, I also linked to my post on Instagram&#8217;s lack of native reposts. <a href="http://nearthespeedoflight.com/article/2016_12_02_fake_news_on_social_networks">Jason Brennan has written a follow-up</a> about fake news and propaganda, exploring what we can learn and apply to microblogging:</p>
+<blockquote><p>
+  Aside from the normal reasons propaganda exists, it exists on social networks like Facebook and Twitter because it <em>can exist</em> on those networks. It’s <em>profitable</em> and <em>useful</em> for the parties manufacturing and disseminating it. To Facebook and Twitter, upon whose networks it propagates, it doesn’t really matter what the information is so long as it <em>engages</em> users. Facebook’s apathy to propaganda is regularly exploited.
+</p></blockquote>
+<p>Hillary Clinton also connected fake news and propaganda <a href="https://www.washingtonpost.com/news/powerpost/wp/2016/12/08/hillary-clinton-attacks-fake-news-in-post-election-appearance-on-capitol-hill/">in a speech this week</a>:</p>
+<blockquote><p>
+  Let me just mention briefly one threat in particular that should concern all Americans, Democrats, Republicans and independents alike, especially those who serve in our Congress: the epidemic of malicious fake news and false propaganda that flooded social media over the past year. It’s now clear that so-called fake news can have real-world consequences.
+</p></blockquote>
+<p>The internet is at a crossroads. Entrepreneurs love free speech, scale, and money, but those don&#8217;t always align in a good way. As much talk as there is of <em>making an impact</em>, very few leaders in Silicon Valley seem to think deeply about consequences.</p>
+]]></content:encoded>
+			</item>
+		<item>
+		<title>Core Intuition 262</title>
+		<link>http://www.manton.org/2016/12/core-intuition-262.html</link>
+		<pubDate>Thu, 08 Dec 2016 18:07:01 +0000</pubDate>
+		<dc:creator><![CDATA[manton]]></dc:creator>
+				<category><![CDATA[Podcasts]]></category>
+		<category><![CDATA[consulting]]></category>
+		<category><![CDATA[coreint]]></category>
+		<category><![CDATA[failure]]></category>
+		<category><![CDATA[goals]]></category>
+
+		<guid isPermaLink="false">http://www.manton.org/?p=4533</guid>
+		<description><![CDATA[We published episode 262 of Core Intuition today. It&#8217;s December already, so we&#8217;ve inevitably been thinking about unfinished projects as the year wraps up. From the show notes: Daniel and Manton talk about coping with disappointment of failing to achieve goals in an expected length of time, recognize the differing demands of building software for [&#8230;]]]></description>
+				<content:encoded><![CDATA[<p>We published <a href="http://coreint.org/262">episode 262 of Core Intuition</a> today. It&#8217;s December already, so we&#8217;ve inevitably been thinking about unfinished projects as the year wraps up. From the show notes:</p>
+<blockquote><p>
+  Daniel and Manton talk about coping with disappointment of failing to achieve goals in an expected length of time, recognize the differing demands of building software for different markets, and talk about tricks for managing lack of enthusiasm for finishing projects. Finally, they answer a listener question about how to get started with consulting, and planning for maintaining a suitable income when you “quit your day job.”
+</p></blockquote>
+<p>Thanks as always for listening to the show.</p>
+]]></content:encoded>
+			</item>
+	</channel>
+</rss>
